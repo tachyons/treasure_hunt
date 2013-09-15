f86d5450c96ced767bcf418f7053a14d4492ca8d
Aboobacker MK <aboobackervyd@gmail.com>
2013-09-15 20:21:17 +0530

    removed 404 issue

--- application/modules/home/views/about.php
+++ application/modules/home/views/about.php
@@ -1,1 +1,9 @@
-hello wold
+<div class="hero-unit">
+	<h1>Adwaita 2013 treasure hunt</h1>
+    <p>Challenge your brain,play with fun </p>
+    	<p>
+    		<a class="btn btn-primary btn-large" href="treasure">
+    		Lets start
+    	</a>
+    </p>
+</div>
