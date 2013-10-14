298cee665eaa266dfb320f398e98854202d6f980
Aboobacker MK <aboobackervyd@gmail.com>
2013-10-14 17:37:55 +0530

    test2

--- application/modules/home/views/about.php
+++
@@ -1,9 +0,0 @@
-<div class="hero-unit">
-	<h1>Adwaita 2013 treasure hunt</h1>
-    <p>Challenge your brain,play with fun </p>
-    	<p>
-    		<a class="btn btn-primary btn-large" href="treasure">
-    		Lets start
-    	</a>
-    </p>
-</div>
